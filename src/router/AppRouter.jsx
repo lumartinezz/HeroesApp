3863c41784ab1067d19f814e006b5017bada0db9
Lucia Macarena Martínez Burroso <luciamacarenamartinez@gmail.com>
2023-01-31 15:37:36 -0300

    adding images

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from '../auth'
-import { HeroesRoutes } from "../../public/heroes" 
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
 import { PrivateRoute } from "./PrivateRoute"
 import { PublicRoute } from "./PublicRoute"
 
