b1e894d740a4e2e55ccb195868d2bc3de3ae1247
Lucia Macarena Martínez Burroso <luciamacarenamartinez@gmail.com>
2023-02-07 16:05:05 -0300

    new build

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -17,7 +17,7 @@ first_appearance,
 characters,
 }) => {
 
-    const heroImageUrl = `docs/heroes/${id}.jpg`
+    const heroImageUrl = `heroes/${id}.jpg`
 
   return (
     <div className="col animate__animated animate__fadeIn">
